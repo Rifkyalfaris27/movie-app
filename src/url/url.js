3f9862a6b8d2718461ff5aaecc1e8b40b40a1845
Rifkyalfaris27 <140361664+Rifkyalfaris27@users.noreply.github.com>
2024-01-04 00:58:14 +0700

    url.js

---
+++ src/url/url.js
@@ -0,0 +1,9 @@
+const baseUrl = 'https://api.themoviedb.org/3/';
+
+const apiKey = 'api_key=0d80e2d66ed07116a2a780790585f845';
+const trending = 'trending/movie/week?'
+
+// render movie by picture
+const baseUrlPicture = 'https://image.tmdb.org/t/p/original'
+
+export {baseUrl, apiKey, trending, baseUrlPicture}
