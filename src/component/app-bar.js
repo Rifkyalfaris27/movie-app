8137eaa0c82525b713097e8307d2c46ea9cdf6a4
Rifkyalfaris27 <140361664+Rifkyalfaris27@users.noreply.github.com>
2024-01-04 00:55:05 +0700

    app-bar.js

---
+++ src/component/app-bar.js
@@ -0,0 +1,9 @@
+// import renderMovie from "../renderMovie";
+import movieOfTheWeek from "../trending";
+
+const homeBtn = document.getElementById('home');
+
+const container = document.querySelector('#container');
+const trending = document.querySelector('.trending');
+const containerMessage = document.querySelector('#responseMessage');
+
